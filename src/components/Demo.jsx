2f222579c50fc1125b20cae698768d63f946e0e8
darkhan-akdauletovich <d.bolatov2402@gmail.com>
2023-05-02 16:36:38 +0600

    06

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -53,9 +53,49 @@ const Demo = () => {
             type="submit" 
             className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵</button>
         </form>
+
         {/*Browse URL History*/}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => {
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate"></p>
+            </div>
+          })}
+        </div>
       </div>
       {/*Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br/>
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+            </div>
+          ) 
+        )}
+      </div>
     </section>
   )
 }
